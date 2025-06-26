c505453bf94038116fd76a04e72dda9e2c885ce4
Cyborg Tek <181218569+DailyInvestors@users.noreply.github.com>
2025-06-26 07:01:54 -0400

    Create Beautifier.sh

---
+++ Beautifier/Beautifier.sh
@@ -0,0 +1,27 @@
+Description: A Beautifier designed to offer fixes for syntax and code completion's. This gives a file audit on what needs to be fixed, addressed, and fixes some general issues.
+
+Requirements:
+1. Linux.
+2. Bash.
+3. Pip install Black.
+4. npm install -g prettier.
+5. go install mvdan.cc/sh/v3/cmd/shfmt@latest.
+
+
+Installation:
+1. Copy our source file.
+2. Touch beautifier.sh
+3. Nano beautifier.sh
+4. Paste in the select destination.
+5. Chmod +X beautifier.sh
+
+
+Activation:
+Python: ./beautifier.sh Python "file_name.txt"
+
+JavaScript/Nodejs: ./beautifier.sh JavaScript "file_name.txt"
+
+Bash: ./beautifier.sh Bash "file_name.txt"
+
+
+We appreciate any and all Donations. We will also support any investors, to help us continue our Work and Research.
